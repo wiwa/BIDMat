d203ca80964a7994ffa47516dc915cc4442f2cbf
Win Wang <fallenpwr@gmail.com>
2019-06-26 11:21:06 -0400

    Building with CUDA 10.1 on Windows

---
+++ Bidmat1.cmd
@@ -0,0 +1,20 @@
+::@ECHO OFF
+:: Set JAVA_HOME here if not set in environment
+:: SET JAVA_HOME= 
+:: Set as much memory as possible
+(SET JAVA_OPTS=-Xmx12G -Xms128M)
+:: Fix these if needed
+SET JCUDA_VERSION=0.6.5
+SET JCUDA_LIBDIR=%CD%\lib
+SET LIBDIR=%CD%\lib
+
+SET BIDMAT_LIBS=%CD%\BIDMat.jar;%LIBDIR%\ptplot.jar;%LIBDIR%\ptplotapplication.jar;%LIBDIR%\jhdf5.jar;%LIBDIR%\commons-math3-3.1.1.jar;%LIBDIR%\lz4-1.1.2.jar
+
+
+SET JCUDA_LIBS=%JCUDA_LIBDIR%\jcuda-%JCUDA_VERSION%.jar;%JCUDA_LIBDIR%\jcublas-%JCUDA_VERSION%.jar;%JCUDA_LIBDIR%\jcufft-%JCUDA_VERSION%.jar;%JCUDA_LIBDIR%\jcurand-%JCUDA_VERSION%.jar;%JCUDA_LIBDIR%\jcusparse-%JCUDA_VERSION%.jar
+
+SET ALL_LIBS=%LIBDIR%\IScala.jar;%BIDMAT_LIBS%;%JCUDA_LIBS%;%JAVA_HOME%\lib\tools.jar
+(SET JAVA_OPTS=-Djava.library.path="%LIBDIR%;%PATH%" %JAVA_OPTS%)
+
+ECHO "%ALL_LIBS%"
+%CD%\scala.bat -toolcp "asd" -Yrepl-sync -i %LIBDIR%\bidmat_init.scala
---
+++ lib/bidmat_init.scala
@@ -0,0 +1,11 @@
+import BIDMat.{BMat,CMat,CSMat,DMat,Dict,FMat,GMat,GDMat,GIMat,GLMat,GSMat,GSDMat,HMat,IDict,Image,IMat,LMat,Mat}
+import BIDMat.{DQuaternion,Quaternion,SMat,SBMat,SDMat,TMat}
+import BIDMat.MatFunctions._
+import BIDMat.SciFunctions._
+import BIDMat.Solvers._
+import BIDMat.Plotting._
+import BIDMat.Mat.console_publish
+
+Mat.checkMKL(false)
+Mat.checkCUDA(true)
+
